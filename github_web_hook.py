10b251acf4242244fb0c492de2be1b306ddde431
Bas Broek <BasThomas@users.noreply.github.com>
2018-08-10 19:49:37 +0200

    Typo fix

--- github_web_hook.py
+++ github_web_hook.py
@@ -9,7 +9,7 @@ PORT = None
 TOKEN = None
 FOLDER = None
 
-# Crazy way of retriving the current address. Only necessary for printing the full address down below..
+# Crazy way of retreiving the current address. Only necessary for printing the full address down below..
 address = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
 
 try:
