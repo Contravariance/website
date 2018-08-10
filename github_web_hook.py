50a4d577540710e8027ff4f5eaa40c774c2a8dea
Benedikt Terhechte <terhechte@gmail.com>
2018-08-10 06:35:13 +0000

    Added webhook

--- github_web_hook.py
+++ github_web_hook.py
@@ -2,10 +2,14 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import SocketServer
 import sys
 import os
+import socket
+
 
 PORT = None
 TOKEN = None
 FOLDER = None
+
+address = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
 
 try:
 	PORT = int(sys.argv[1])
@@ -23,19 +27,16 @@ class S(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-    def do_GET(self):
+    def do_POST(self):
         self._set_headers()
 	print self.path
+        self.wfile.write("")
 	if self.path == "/hook/%s" % (TOKEN,):
-		self.wfile.write("")
-		os.system("""
-cd %s
+		os.system("""cd %s
 git reset --hard
-git pull
-""" % (FOLDER,)
-	else:
-		self.wfile.write("")
+git pull""" % (FOLDER,))
 
+print("webhook url: http://%s:%s/hook/%s" % (address, PORT, TOKEN))
 server_address = ('', PORT)
 httpd = HTTPServer(server_address, S)
 httpd.serve_forever()
